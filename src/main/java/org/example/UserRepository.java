b49211973ae59078b669dec99b54e21a9f6511e0
CodeHacker23 <ilarion0@yandex.ru>
2025-06-25 11:09:07 +0500

    Сделал в своем роде модуляцию проекта, вроде бы все работает

---
+++ src/main/java/org/example/UserRepository.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {// что бы вщаимодействовать с сушностью и она конекит нас с БД
+
+    UserEntity findByTgId(Long tgId);
+
+    UserEntity findByUsername(String username);
+
+}
