29bdf2d9712d3dfcd532fec1c3b12106a701a2fb
CodeHacker23 <ilarion0@yandex.ru>
2025-06-26 11:26:06 +0500

    1

---
+++ TG_BOT_PRAKTIKA_18_05_25-larik/src/main/java/org/example/Config.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class Config {
+
+    @Bean
+    TelegramBotsApi telegramBotsApi(Bot bot) {
+        TelegramBotsApi telegramBotsApi;
+        try {
+            telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException();
+        }
+        return telegramBotsApi;
+    }
+}
